1c0fbbed29da22eb70ed34ec573e2fe68e19a03d
Kamilly Mota <kamillybmota15@gmail.com>
2025-01-26 18:12:38 -0300

    implantação da lógica completa

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let amigos = [];
+let sorteados = [];
 
 function adicionarAmigo(){
-    let novoAmigo = document.getElementById('amigo').value;
+    let novoAmigo = document.getElementById("amigo").value;
     if (novoAmigo != ''){
         amigos.push(novoAmigo);
         exibirAmigos();
@@ -12,23 +13,34 @@ function adicionarAmigo(){
 }
 
 function limparCampos(){
-    let novoAmigo = document.getElementById('amigo');
+    let novoAmigo = document.getElementById("amigo");
     novoAmigo.value = '';
 }
+
+
 function exibirAmigos (){
     let listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = '';
     for (let a = 0; a < amigos.length; a++) {
-    const amigo = document.createElement('li');
+    const amigo = document.createElement("li");
     amigo.textContent = amigos[a];
-    amigos.appendChild(amigo);
+    listaAmigos.appendChild(amigo);
   }
-}
-function exibirAlgo(tag, texto){
-    let campo = document.getElementById(tag);
-    campo.innerHTML = texto;
 }
 
 function sortearAmigo(){
-    console.log('funcao vazia');
+    if (amigos.length == 0) {
+        alert("Insira alguns nomes para sorteá-los!");
+      } else {
+        if (sorteados.length == amigos.length){
+            alert("Todos os amigos já foram sorteados!")
+        }
+        let escolhido = Math.floor(Math.random() * amigos.length);
+        while (sorteados.includes(amigos[escolhido])){
+            escolhido = Math.floor(Math.random()* amigos.length);
+        }
+        sorteados.push(amigos[escolhido]);
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = `Seu amigo secreto é: ${amigos[escolhido]}!`;
+      }
 }
